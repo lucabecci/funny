ce16cd7f53e21791df3d7475fbe089e70eecd5ae
lucabecci <beccibrian@gmail.com>
2021-11-03 01:17:55 -0300

    fix(ArrayLib): tdd generator fixed

--- libs/ArrayLib_test.go
+++ libs/ArrayLib_test.go
@@ -1,10 +1,38 @@
 package libs
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 )
 
+type arrayLibTest struct {
+	longitude int
+	increment int
+}
+
+var arrayLibTests = []arrayLibTest{
+	arrayLibTest{
+		30,
+		200,
+	},
+}
+
 var arrayLib ArrayLib
+
+func init() {
+	arrayLib = ArrayLib{
+		min: 10,
+		max: 200,
+	}
+	for i := 0; i <= 5; i++ {
+		arrayLibTests = append(arrayLibTests, arrayLibTest{
+			longitude: rand.Intn(200-5) + 5,
+			increment: rand.Intn(4000-10) + 10,
+		})
+	}
+	fmt.Println(arrayLibTests)
+}
 
 func TestRandomArray(t *testing.T) {
 	arrayLib = ArrayLib{
