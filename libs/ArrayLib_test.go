7a1b89e4d1989e19c663f1fb4137e899b39f1612
lucabecci <beccibrian@gmail.com>
2021-11-04 21:57:15 -0300

    feat(): binary search implemented in cpp

--- libs/ArrayLib_test.go
+++ libs/ArrayLib_test.go
@@ -6,55 +6,25 @@ import (
 	"testing"
 )
 
-type arrayLibTest struct {
-	longitude int
-	increment int
-}
-
-var arrayLibTests = []arrayLibTest{
-	arrayLibTest{
-		30,
-		200,
-	},
-}
-
-var arrayLib ArrayLib
+var arrayLibTests = []ArrayLib{}
 
 func init() {
-	arrayLib = ArrayLib{
-		min: 10,
-		max: 200,
-	}
 	for i := 0; i <= 5; i++ {
-		arrayLibTests = append(arrayLibTests, arrayLibTest{
-			longitude: rand.Intn(200-5) + 5,
-			increment: rand.Intn(4000-10) + 10,
+		arrayLibTests = append(arrayLibTests, ArrayLib{
+			max: rand.Intn(4000-5) + 5,
+			min: rand.Intn(20-10) + 10,
 		})
 	}
 	fmt.Println(arrayLibTests)
 }
 
 func TestRandomArray(t *testing.T) {
-	arrayLib = ArrayLib{
-		min: 5,
-		max: 15,
-	}
-	arr := arrayLib.RandomArray(20)
-	if len(arr) != 20 {
-		t.Fatal("expected", 20)
-		t.Fatal("received", len(arr))
+	for _, test := range arrayLibTests {
+		if out := test.RandomArray(200); len(out) != 200 {
+			t.Fatal(out)
+		}
 	}
 }
 
 func TestRandomIncrementArray(t *testing.T) {
-	arrayLib = ArrayLib{
-		min: 7,
-		max: 200,
-	}
-
-	arr := arrayLib.RandomIncrementArray(20, 800)
-	if len(arr) != 20 {
-		t.Fatal("expected", 20)
-		t.Fatal("received", len(arr))
-	}
 }
