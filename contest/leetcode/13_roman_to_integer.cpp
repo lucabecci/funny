b5466077e0dd16d193e3fb4066f45b19ef86ad94
lucabecci <beccibrian@gmail.com>
2021-11-08 22:07:11 -0300

    feat(): stack array implementation

---
+++ contest/leetcode/13_roman_to_integer.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<string>
+
+int main(int argc, char *argv[])
+{
+  std::cout << "Initial process" << std::endl;
+
+
+}
+
+
+
+
+int findValue(std::string & value)
+{
+  if(v == "I") return 1;
+  else if(v == "V") return 5;
+}
---
+++ dsa/stack/array_main.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+
+
+//stack of 20 v
+class Stack
+{
+  private:
+    int top;
+
+  public:
+    int a[20];
+
+    Stack() {top = -1;}
+    bool push(int x);
+    int pop();
+    int peek();
+    bool isEmpty();
+};
+
+bool Stack::push(int x)
+{
+    if(top >= (20 - 1))
+    {
+      std::cout << "Stack overflow!" << std::endl;
+      return false;
+    }
+    else
+    {
+      a[++top] = x;
+      std::cout << x << " pushed into stack" << std::endl;
+      return true;
+    }
+}
+
+
+int Stack::pop()
+{
+  if(isEmpty())
+  {
+    std::cout << "Stack underflow!" << std::endl;
+    return 0;
+  }
+  else
+  {
+    int x = a[top--];
+    return x;
+  }
+}
+
+
+int Stack::peek()
+{
+  if(isEmpty())
+  {
+    std::cout << "Stack underflow!" << std::endl;
+    return 0;
+  }
+  else
+  {
+    int x = a[top];
+    return x;
+  }
+}
+
+bool Stack::isEmpty()
+{
+  if(top < 0)
+  {
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  Stack c;
+  c.push(22);
+  c.push(44);
+  c.push(1042);
+  std::cout << "Before the pop: " << c.peek() << std::endl;
+  c.pop();
+  std::cout << "After the pop: " << c.peek() << std::endl;
+  return 0;
+}
