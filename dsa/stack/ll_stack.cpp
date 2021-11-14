7e4f80e6392c86864b610bef19c019766f55586b
lucabecci <beccibrian@gmail.com>
2021-11-14 11:40:35 -0300

    feat(): stack ll implemented

--- dsa/stack/ll_stack.cpp
+++ dsa/stack/ll_stack.cpp
@@ -2,15 +2,77 @@
 
 using namespace std;
 
-class StackNode {
+class StackNode 
+{
   public:
     int data;
     StackNode* next;
 };
 
-class Stack {
-  private:
-    StackNode* root;
+class ProcessStackLL
+{
   public:
-    StackNode;
+    StackNode* newNode(int data)
+    {
+      StackNode* stackNode = new StackNode();
+      stackNode->data = data;
+      stackNode->next = NULL;
+      return stackNode;
+    }
+    int isEmpty(StackNode* root)
+    {
+      return !root;
+    }
+    void push(StackNode** root, int data)
+    {
+      StackNode* stackNode = newNode(data);
+      stackNode->next = *root;
+      *root = stackNode;
+      std::cout << data << " pushed into stack" << std::endl;
+    }
+    int pop(StackNode** root)
+    {
+      if( isEmpty(*root) )
+      {
+        return INT_MIN;
+      }
+      StackNode* temp = *root;
+      *root = (*root)->next;
+      int popped = temp->data;
+      free(temp);
+
+      return popped;
+    }
+
+    int peek(StackNode* root)
+    {
+      if(isEmpty(root))
+      {
+        return INT_MIN;
+      }
+      return root->data;
+    }
 };
+
+
+
+int main(int argc, char *argv[])
+{
+  StackNode* root = NULL;
+  ProcessStackLL sll;
+  sll.push(&root, 200);
+  sll.push(&root, 3030);
+  sll.push(&root, 1);
+  
+
+  int popped = sll.pop(&root);
+  std::cout << popped << " deleted of the ll stack" << std::endl;
+  std::cout << "Top element is: " << sll.peek(root) << std::endl;
+
+  while(!sll.isEmpty(root))
+  {
+    std::cout << sll.peek(root) << std::endl;
+    sll.pop(&root);
+  }
+  return 0;
+}
