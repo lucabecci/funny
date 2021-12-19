88564226672d980a8e8fdf2f774d872939e05e04
lucabecci <beccibrian@gmail.com>
2021-12-19 19:01:52 -0300

    feat(): deletion in binary tree

--- cpp/dsa/binary_tree/main.cpp
+++ cpp/dsa/binary_tree/main.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include <iterator>
 
 struct Node
 {
@@ -57,6 +58,86 @@ Node* InsertNode(Node* root, int data)
   }
 }
 
+void DeleteDeepest(Node* root, Node* d_node)
+{
+  std::queue<Node*> q;
+  q.push(root);
+  Node* temp;
+  while(!q.empty())
+  {
+    temp = q.front();
+    q.pop();
+    if(temp == d_node)
+    {
+      temp = NULL;
+      delete (d_node);
+      return;
+    }
+    if(temp->right)
+    {
+      if(temp->right == d_node)
+      {
+        temp->right = NULL;
+        delete (d_node);
+        return;
+      }
+      else 
+      {
+        q.push(temp->right);
+      }
+    }
+  }
+  if(temp->left)
+  {
+    if(temp->left == d_node)
+    {
+      temp->left = NULL;
+      delete (d_node);
+      return;
+    }
+    else
+    {
+      q.push(temp->left);
+    }
+  }
+}
+
+Node* Deletion(Node* root, int value)
+{
+  if(root == NULL) return NULL;
+  if(root->left == NULL && root->right == NULL)
+  {
+    if(root->data == value) return NULL;
+    else return root;
+  }
+
+  std::queue<Node*> q;
+  q.push(root);
+
+  Node* temp;
+  Node* key_node = NULL;
+
+  while(!q.empty())
+  {
+    temp = q.front();
+    q.pop();
+
+    if(temp->data == value) key_node = temp;
+
+    if(temp->left) q.push(temp->left);
+
+    if(temp->right) q.push(temp->right);
+  }
+
+  if(key_node != NULL)
+  {
+    int x = temp->data;
+    DeleteDeepest(root, temp);
+    key_node->data = x;
+  }
+  return root;
+}
+
 void InOrder(Node* temp)
 {
   if(temp == NULL)
@@ -70,21 +151,24 @@ void InOrder(Node* temp)
 
 int main(int argc, char *argv[])
 {
-  Node* root = CreateNode(0);
-  root->left = CreateNode(1);
-  root->right = CreateNode(2);
-  root->left->left = CreateNode(3);
-  root->left->right = CreateNode(4);
-  root->right->left = CreateNode(5);
-  root->right->right = CreateNode(6);
+
+  struct Node* root = CreateNode(10);
+  root->left = CreateNode(11);
+  root->left->left = CreateNode(7);
+  root->left->right = CreateNode(12);
+  root->right = CreateNode(9);
+  root->right->left = CreateNode(15);
+  root->right->right = CreateNode(8);
+
+  std::cout << "Inorder traversal before deletion : ";
   InOrder(root);
-  std::cout << "" << std::endl;
-  int v = 7;
-  root = InsertNode(root, v);
+
+  int key = 11;
+  root = Deletion(root, key);
+
+  std::cout << std::endl;
+  std::cout << "Inorder traversal after deletion : ";
   InOrder(root);
-  std::cout << "" << std::endl;
-  int v2 = 8;
-  root = InsertNode(root, v2);
-  InOrder(root);
+
   return 0;
 }
